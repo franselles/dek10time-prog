b675087c9ff9776ab30812d579c72f67972e7d1e
franselles <fran.selles@gmail.com>
2020-04-10 11:26:43 +0200

    Fixed package and vars

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -29,7 +29,7 @@
                   LOGIN
                 </button>
               </div>
-              <a href="#" class="float-right">1.0.7</a>
+              <a href="#" class="float-right">1.0.8</a>
             </form>
           </div>
         </div>
